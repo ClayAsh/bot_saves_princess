a35286c87a29a07c68d200f6fe2adac39dffc9ed
Clay Ash <claycourtneyash@gmail.com>
2022-09-08 07:51:59 -0600

    Add find_princess method and test to return princess coordinates

--- save_princess/lib/grid.rb
+++ save_princess/lib/grid.rb
@@ -2,7 +2,19 @@ class Grid
   attr_reader :size,
               :grid 
   def initialize(size, grid)
+    #wrong grid size(2 >, > 100 && odd), princess not found, bot not found
     @size = size 
     @grid = grid
   end
+
+  def find_princess 
+    coordinates = []
+    @grid.each_with_index do |row, index|
+      if row.include?('p')
+        coordinates << index 
+        coordinates << row.chars.index('p')
+      end
+    end
+    coordinates
+  end 
 end
--- save_princess/spec/grid_spec.rb
+++ save_princess/spec/grid_spec.rb
@@ -2,13 +2,19 @@ require_relative '../lib/grid'
 require 'rspec'
 
 RSpec.describe Grid do 
+  let(:grid) { Grid.new(3, ["---", "-m-", "p--"]) } 
+
   describe 'initialize' do 
     it 'exists and has attributes' do 
-      grid = Grid.new(3, ["---", "-m-", "p--"])
-
       expect(grid).to be_a(Grid)
       expect(grid.size).to eq(3)
       expect(grid.grid).to eq(["---", "-m-", "p--"])
+    end
+  end
+
+  describe '.find_princess' do 
+    it 'can return princess coordinates' do 
+      expect(grid.find_princess).to eq([2, 0])
     end
   end
 end
