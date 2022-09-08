fbeb3dc71fb9fc6e418e510ab83dc361c5199575
Clay Ash <claycourtneyash@gmail.com>
2022-09-08 08:35:24 -0600

    Add ArgumentErrors and sad path test for incorrect user input

--- save_princess/lib/grid.rb
+++ save_princess/lib/grid.rb
@@ -2,9 +2,12 @@ class Grid
   attr_reader :size,
               :grid 
   def initialize(size, grid)
-    #wrong grid size(2 >, > 100 && odd), princess not found, bot not found
+    raise ArgumentError, "Please enter an odd number between 3 & 100" unless size >= 3 && size <= 100
+    raise ArgumentError, "Please enter an odd number" unless size.odd? == true 
+    raise ArgumentError, "Please enter the correct number of columns for your grid size" unless grid.all? {|e| e.length == size} == true
     @size = size 
     @grid = grid
+
   end
 
   def find_princess 
--- save_princess/spec/grid_spec.rb
+++ save_princess/spec/grid_spec.rb
@@ -11,11 +11,35 @@ RSpec.describe Grid do
       expect(grid.grid).to eq(["---", "-m-", "p--"])
     end
   end
-
-  describe '.find_princess' do 
+  
+  describe 'find_princess' do 
     it 'can return princess coordinates' do 
       expect(grid.find_princess).to eq([2, 0])
     end
+  end
+  
+  describe 'sad path' do 
+    it 'returns an error if grid size is greater than 100' do 
+      expect {Grid.new(101, ["---", "-m-", "p--"])}.to raise_error(ArgumentError)
+    end
+
+    
+    it 'returns an error if grid size is less than 3' do 
+      expect {Grid.new(1, ["-", "m", "p"])}.to raise_error(ArgumentError)
+    end
+    
+    it 'returns an error if grid size is even' do 
+      expect {Grid.new(4, ["----", "-m--", "p---"])}.to raise_error(ArgumentError)
+    end
+    
+    it 'returns an error if grid size and grid do not match' do 
+      expect {Grid.new(3, ["-----", "--m--", "p----"])}.to raise_error(ArgumentError)
+    end
+
+    it 'doesnt return error for correct size grids over 3' do 
+      expect { Grid.new(5, ["-----", "-----", "--m--","-----", "p----"]) }.not_to raise_error
+    end
+
   end
 end
 
