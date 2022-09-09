b4e48415cc75e64c36c6145ed58f445878f63a6e
Clay Ash <claycourtneyash@gmail.com>
2022-09-08 19:17:35 -0600

    Remove extranious code in HR save princess for Hacker Rank terminal

--- HR_save_princess.rb
+++ HR_save_princess.rb
@@ -64,22 +64,6 @@ class SavePrincess
   end
 end 
 
-puts "Please enter a number between 3 & 100 to choose the grid size."
-grid_size = gets.chomp.to_i
-grid = []
-row = 1
-grid_size.times do 
-  puts "Please enter row #{row} for your grid. Please place 'p' in a corner and 'm' in the middle of the grid."
-  grid << gets.chomp 
-  row +=1
-end
-
-new_grid = Grid.new(grid_size, grid)
-new_game = SavePrincess.new(new_grid)
-new_game.find_path.each do |e|
-  p e
-end
-
 def displayPathtoPrincess(n,grid)
   new_grid = Grid.new(n, grid)
   new_game = SavePrincess.new(new_grid)
